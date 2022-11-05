f789f32f2eb9c8aee8c2ccae2b4b40b67572ffc4
Abdillah Uwesu Abubakar <aabubakar@softnet.co.tz>
2022-11-05 09:09:30 +0300

    Second Changes

---
+++ SigmaHubApi/Modal/Candidate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SigmaHubApi.Modal
+{
+    public class Candidate
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public TimeSpan TimeInterval { get; set; }
+        public string LinkedInURL { get; set; }
+        public string GitHubURL { get; set; }
+        [Required]
+        public string FreeTextComment { get; set; }
+    }
+}
